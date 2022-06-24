757e46dc6566e34f8d22f18b615b1209aed6da8c
surbot-1 <99324745+surbot-1@users.noreply.github.com>
2022-06-24 17:34:08 +0530

    Update client.js

--- client.js
+++ client.js
@@ -2,7 +2,55 @@
 
 
 
-
+function handleLogin(success) { 
+   if (success === false) { 
+      alert("Ooops...try a different username"); 
+   } else { 
+      loginPage.style.display = "none"; 
+      callPage.style.display = "block"; 
+		
+      //********************** 
+      //Starting a peer connection 
+      //********************** 
+		
+      //getting local audio stream 
+      navigator.webkitGetUserMedia({ video: false, audio: true }, function (myStream) { 
+         stream = myStream; 
+			
+         //displaying local audio stream on the page 
+         localAudio.src = window.URL.createObjectURL(stream);
+			
+         //using Google public stun server 
+         var configuration = { 
+            "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }] 
+         }; 
+			
+         yourConn = new webkitRTCPeerConnection(configuration); 
+			
+         // setup stream listening 
+         yourConn.addStream(stream); 
+			
+         //when a remote user adds stream to the peer connection, we display it 
+         yourConn.onaddstream = function (e) { 
+            remoteAudio.src = window.URL.createObjectURL(e.stream); 
+         }; 
+			
+         // Setup ice handling 
+         yourConn.onicecandidate = function (event) { 
+            if (event.candidate) { 
+               send({ 
+                  type: "candidate", 
+                  candidate: event.candidate 
+               }); 
+            } 
+         }; 
+			
+      }, function (error) { 
+         console.log(error); 
+      }); 
+		
+   } 
+};
 
 //initiating a call 
 callBtn.addEventListener("click", function () { 
